487939875b56fd54708a5e815792fdcdafa26e9c
Krakozaybr <krakozaybp@gmail.com>
2024-08-16 20:41:37 +0700

    Added CurrencyListStoreFactory to di

--- presentation/navigation/src/main/java/com/krakozaybr/navigation/di/NavigationModule.kt
+++ presentation/navigation/src/main/java/com/krakozaybr/navigation/di/NavigationModule.kt
@@ -2,6 +2,7 @@ package com.krakozaybr.navigation.di
 
 import com.arkivanov.mvikotlin.core.store.StoreFactory
 import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
+import com.krakozaybr.navigation.currency_list.CurrencyListStoreFactory
 import com.krakozaybr.navigation.currency_list.DefaultCurrencyListComponent
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
@@ -9,5 +10,6 @@ import org.koin.dsl.module
 
 val navigationModule = module {
     factoryOf<StoreFactory>(::DefaultStoreFactory)
+    factoryOf(::CurrencyListStoreFactory)
     factoryOf(DefaultCurrencyListComponent::Factory)
 }
