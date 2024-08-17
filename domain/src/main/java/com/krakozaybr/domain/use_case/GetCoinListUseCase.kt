603c95cdc31dbfa131b6106d7735cfc5f0f652d8
Krakozaybr <krakozaybp@gmail.com>
2024-08-17 10:44:35 +0700

    Add currency param to GetCoinListUseCase

--- domain/src/main/java/com/krakozaybr/domain/use_case/GetCoinListUseCase.kt
+++ domain/src/main/java/com/krakozaybr/domain/use_case/GetCoinListUseCase.kt
@@ -1,11 +1,12 @@
 package com.krakozaybr.domain.use_case
 
+import com.krakozaybr.domain.model.Currency
 import com.krakozaybr.domain.repository.CoinRepository
 
 class GetCoinListUseCase(
     private val repository: CoinRepository
 ) {
 
-    operator fun invoke() = repository.getCoins()
+    operator fun invoke(currency: Currency) = repository.getCoins(currency)
 
 }
