b496b88767558fe9ba6e604dc8cfdd1bcad840dd
Krakozaybr <krakozaybp@gmail.com>
2024-08-17 10:40:38 +0700

    Add currency param to CoinRepository.getCoins

--- domain/src/main/java/com/krakozaybr/domain/repository/CoinRepository.kt
+++ domain/src/main/java/com/krakozaybr/domain/repository/CoinRepository.kt
@@ -2,6 +2,7 @@ package com.krakozaybr.domain.repository
 
 import com.krakozaybr.domain.model.CoinDetails
 import com.krakozaybr.domain.model.CoinInfo
+import com.krakozaybr.domain.model.Currency
 import com.krakozaybr.domain.resource.FailureReason
 import com.krakozaybr.domain.resource.Resource
 import kotlinx.collections.immutable.ImmutableList
@@ -9,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface CoinRepository {
 
-    fun getCoins(): Flow<Resource<ImmutableList<CoinInfo>, FailureReason>>
+    fun getCoins(currency: Currency): Flow<Resource<ImmutableList<CoinInfo>, FailureReason>>
 
     fun getCoinDetails(id: String): Flow<Resource<CoinDetails, FailureReason>>
 
