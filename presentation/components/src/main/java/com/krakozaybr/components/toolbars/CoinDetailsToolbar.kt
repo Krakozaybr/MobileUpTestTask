7f2fe27eb923ec47168ceb0ee1f9eb20abbdf01d
Krakozaybr <krakozaybp@gmail.com>
2024-08-17 20:02:28 +0700

    CoinDetailsToolbar

---
+++ presentation/components/src/main/java/com/krakozaybr/components/toolbars/CoinDetailsToolbar.kt
@@ -0,0 +1,49 @@
+package com.krakozaybr.components.toolbars
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.krakozaybr.components.theme.AppTheme
+
+@Composable
+fun CoinDetailsToolbar(
+    title: String,
+    onGoBackClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    spacing: Dp = 20.dp,
+) {
+    AppToolbar(
+        modifier = modifier
+    ) {
+
+        Row (
+            modifier = Modifier,
+            horizontalArrangement = Arrangement.spacedBy(spacing),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            GoBackToolbarIcon(onClick = onGoBackClick)
+            ToolbarText(text = title)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ToolbarTextPreview() {
+    AppTheme {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CoinDetailsToolbar(
+                onGoBackClick = {},
+                title = "Some title",
+                modifier = Modifier
+            )
+        }
+    }
+}
