e3234394b91e2331e0ea3252d1db5410190f13c7
Krakozaybr <krakozaybp@gmail.com>
2024-08-19 08:58:56 +0700

    Add theme call and animation to RootScreen

--- presentation/screens/src/main/java/com/krakozaybr/screens/RootScreen.kt
+++ presentation/screens/src/main/java/com/krakozaybr/screens/RootScreen.kt
@@ -3,6 +3,9 @@ package com.krakozaybr.screens
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.extensions.compose.stack.Children
+import com.arkivanov.decompose.extensions.compose.stack.animation.slide
+import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
+import com.krakozaybr.components.theme.AppTheme
 import com.krakozaybr.navigation.root.RootComponent
 import com.krakozaybr.navigation.root.RootComponent.Child.CoinDetails
 import com.krakozaybr.navigation.root.RootComponent.Child.CoinList
@@ -12,13 +15,16 @@ fun RootScreen(
     component: RootComponent,
     modifier: Modifier = Modifier
 ) {
-    Children(
-        modifier = modifier,
-        stack = component.stack
-    ) {
-        when (val instance = it.instance) {
-            is CoinDetails -> CoinDetailsScreen(component = instance.component)
-            is CoinList -> CoinListScreen(component = instance.component)
+    AppTheme {
+        Children(
+            modifier = modifier,
+            stack = component.stack,
+            animation = stackAnimation(slide())
+        ) {
+            when (val instance = it.instance) {
+                is CoinDetails -> CoinDetailsScreen(component = instance.component)
+                is CoinList -> CoinListScreen(component = instance.component)
+            }
         }
     }
 }
