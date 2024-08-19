976dbde03c0c97e8d9190abf8cf27d493b224b0a
Krakozaybr <krakozaybp@gmail.com>
2024-08-19 08:59:55 +0700

    Add crossfade to CoinListScreen lists, display currency loader at center

--- presentation/screens/src/main/java/com/krakozaybr/screens/CoinListScreen.kt
+++ presentation/screens/src/main/java/com/krakozaybr/screens/CoinListScreen.kt
@@ -1,8 +1,11 @@
 package com.krakozaybr.screens
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
@@ -10,6 +13,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.krakozaybr.components.coins.CoinItemList
 import com.krakozaybr.components.currency.CurrencyChipList
 import com.krakozaybr.components.error.ErrorBanner
@@ -37,24 +41,41 @@ fun CoinListScreen(
             }
         }
 
-        CoinListToolbar {
+        CoinListToolbar(
+            modifier = Modifier
+        ) {
             val currencyState by remember {
                 derivedStateOf {
                     model.currencyState
                 }
             }
 
-            when (val it = currencyState) {
-                LoadFailed -> {}
-                Loading -> Loader()
-                is LoadSuccess -> {
-                    CurrencyChipList(
-                        selected = selectedCurrency,
-                        onChipClick = component::onSelectCurrency,
-                        currencyList = it.currencies
-                    )
+            Crossfade(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                targetState = currencyState,
+                label = "Currency list state crossfade"
+            ) {
+                Box(
+                    modifier = Modifier
+                        .height(80.dp)
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    when (it) {
+                        LoadFailed -> {}
+                        Loading -> Loader()
+                        is LoadSuccess -> {
+                            CurrencyChipList(
+                                selected = selectedCurrency,
+                                onChipClick = component::onSelectCurrency,
+                                currencyList = it.currencies
+                            )
+                        }
+                    }
                 }
             }
+
         }
 
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
@@ -63,21 +84,25 @@ fun CoinListScreen(
                     model.coinState
                 }
             }
-            when (val it = coinListState) {
-                CoinState.LoadFailed -> {
-                    ErrorBanner(onRetryClick = component::reloadAll)
-                }
 
-                CoinState.Loading -> Loader()
-                is CoinState.LoadSuccess -> {
-                    CoinItemList(
-                        onCoinClick = component::onShowDetails,
-                        currency = selectedCurrency
-                            ?: throw RuntimeException("Currency cannot be null if coins are loaded"),
-                        coinList = it.coins
-                    )
+            Crossfade(targetState = coinListState, label = "Coin list state crossfade") {
+                when (it) {
+                    CoinState.LoadFailed -> {
+                        ErrorBanner(onRetryClick = component::reloadAll)
+                    }
+
+                    CoinState.Loading -> Loader()
+                    is CoinState.LoadSuccess -> {
+                        CoinItemList(
+                            onCoinClick = component::onShowDetails,
+                            currency = selectedCurrency
+                                ?: throw RuntimeException("Currency cannot be null if coins are loaded"),
+                            coinList = it.coins
+                        )
+                    }
                 }
             }
+
         }
 
     }
