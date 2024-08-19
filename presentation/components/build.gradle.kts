1e810ebee9cc6c63f0d2fd8cb7812cb4f84e356b
Krakozaybr <krakozaybp@gmail.com>
2024-08-19 10:55:45 +0700

    Added html support for coin details

--- presentation/components/build.gradle.kts
+++ presentation/components/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation(libs.coil)
     implementation(libs.coil.compose)
 
+    implementation(libs.html.text)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
