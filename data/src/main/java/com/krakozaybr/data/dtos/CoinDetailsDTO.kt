6583f99e624b9a347edc7e5fe679ccd41fa90812
Krakozaybr <krakozaybp@gmail.com>
2024-08-19 10:51:41 +0700

    Now description is loaded correctly

--- data/src/main/java/com/krakozaybr/data/dtos/CoinDetailsDTO.kt
+++ data/src/main/java/com/krakozaybr/data/dtos/CoinDetailsDTO.kt
@@ -5,6 +5,7 @@ import kotlinx.collections.immutable.toImmutableList
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import java.util.Locale
 
 @Serializable
@@ -21,12 +22,21 @@ internal data class CoinDetailsDTO(
     fun map() = CoinDetails(
         id = id,
         imageLink = imageLink.large,
-        description = description[Locale.getDefault().language]?.toString()
-            ?: description["en"]?.toString()
+        description = description(Locale.getDefault().language)
+            ?: description("en")
             ?: "",
         categories = categories.toImmutableList()
     )
+
+    private fun description(locale: String): String? {
+        return description[locale]
+            ?.jsonPrimitive
+            ?.takeIf { it.isString }
+            ?.content
+            ?.takeIf { it.isNotEmpty() && it.isNotBlank() }
+    }
 }
+
 @Serializable
 internal data class ImageHolder(
     @SerialName("thumb")
